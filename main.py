f262921a188f00e7edaea101bae53ad3ea0db37c
kawada1 <110166103+kawada1@users.noreply.github.com>
2023-12-14 16:08:14 +0900

    commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import modules.SayHello as App
+from diaries.DiarySample import DiarySample
 
-def run():
-    app = App.SayHello("Git")
-    app.say()
-if __name__ == '__main__':
-    run()
+# ↓のリストには、メンバーの各日記が格納されます。
+diaries = [DiarySample(), ]
+
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
+
+# def run():
+#     app = App.SayHello("Git")
+#     app.say()
+# if __name__ == '__main__':
+#     run()
